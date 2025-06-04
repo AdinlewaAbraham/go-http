c8fa11923d058ee460cbc012f0936d62703a8ee8
abraham <abrahamadinlewa@gmail.com>
2025-06-04 18:18:44 +0100

    fix

--- app/server.go
+++ app/server.go
@@ -23,6 +23,7 @@ func main() {
 	httpServer.Get("/", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		res.Status(200).Send([]byte("cool"))
 	})
+
 	httpServer.Get("/ping", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		buff := bytes.Buffer{}
 
@@ -35,6 +36,7 @@ func main() {
 
 		res.Send(buff.Bytes())
 	})
+
 	httpServer.Get("/echo/:id", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		id, exists := req.Params["id"]
 		if !exists {
@@ -45,10 +47,12 @@ func main() {
 		res.Status(200).SetHeader(string(constants.HeaderKeyContentType), string(constants.ContentTypeText))
 		res.Send([]byte(id))
 	})
+
 	httpServer.Get("/user-agent", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		res.SetHeader("Content-Type", "text/plain")
 		res.Status(200).Send([]byte(req.UserAgent))
 	})
+
 	httpServer.Get("/files/:filename", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		if *dir == "" {
 			fmt.Println("Error: --directory flag is required")
@@ -75,6 +79,7 @@ func main() {
 		res.SetHeader(string(constants.HeaderKeyContentType), string(constants.ContentTypeOctet))
 		res.SendFile(req.Params["filename"], fileData)
 	})
+
 	httpServer.Post("/files/:filename", func(req *httpx.HttpRequest, res *httpx.HttpResponse) {
 		if *dir == "" {
 			fmt.Println("Error: --directory flag is required")
@@ -113,66 +118,4 @@ func main() {
 	httpServer.Listen("0.0.0.0:4221", func() {
 		fmt.Println("listen callback")
 	})
-
 }
-
-// func handleConnection(conn net.Conn, dir *string) {
-// 	defer conn.Close()
-
-// 	buff := make([]byte, 1024)
-// 	_, err := conn.Read(buff)
-// 	if err != nil {
-// 		fmt.Println("Read error:", err)
-// 		return
-// 	}
-
-// 	_, _, path, userAgent, extractUrlErr := util.ExtractUrl(buff)
-// 	if extractUrlErr != nil {
-// 		fmt.Println("Error extracting url:", extractUrlErr)
-// 		return
-// 	}
-
-// 	if strings.HasPrefix(path, "/echo/") {
-// 		res := ""
-// 		pathParts := strings.Split(path, "/echo/")
-// 		if len(pathParts) >= 2 {
-// 			res = pathParts[1]
-// 		}
-
-// 		conn.Write([]byte(util.BuildHTTPResponse(200, constants.ContentTypeText, res)))
-// 	} else if strings.HasPrefix(path, "/user-agent") {
-// 		conn.Write([]byte(util.BuildHTTPResponse(200, constants.ContentTypeText, userAgent)))
-// 	} else if strings.HasPrefix(path, "/files") {
-// 		filePathParts := strings.Split(path, "/files/")
-// 		if len(filePathParts) < 2 {
-// 			conn.Write([]byte(util.BuildHTTPResponse(constants.StatusBadRequest, constants.ContentTypeText, "shit request fix up")))
-// 			return
-// 		}
-// 		filename := strings.Trim(filePathParts[1], "/")
-
-// 		if *dir == "" {
-// 			fmt.Println("Error: --directory flag is required")
-// 			return
-// 		}
-
-// 		fullPath := filepath.Join(*dir, filename)
-
-// 		fileData, err := os.ReadFile(fullPath)
-// 		if err != nil {
-// 			if os.IsNotExist(err) {
-// 				conn.Write([]byte(util.BuildHTTPResponse(constants.StatusNotFound, constants.ContentTypeText, "File not found")))
-// 			} else {
-// 				conn.Write([]byte(util.BuildHTTPResponse(constants.StatusInternalServerError, constants.ContentTypeText, "Internal server error")))
-// 			}
-// 			return
-// 		}
-
-// 		conn.Write([]byte(util.BuildHTTPResponse(constants.StatusOK, constants.ContentTypeOctet, string(fileData))))
-// 	} else {
-// 		if path != "/" {
-// 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-// 		} else {
-// 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-// 		}
-// 	}
-// }
